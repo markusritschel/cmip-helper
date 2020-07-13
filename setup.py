47e08e9dd3548acb11eaa412ae14456161be344d
markusritschel <git@markusritschel.de>
2020-07-13 15:02:56 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cmip-helper',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This package provides various routines to help dealing with CMIP data',
+    author='Markus Ritschel',
+    license='',
+)
